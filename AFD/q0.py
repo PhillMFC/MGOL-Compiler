6a6e8d304e01473a75671972d91e5bc688137816
tibia-sv <dimitrisveloso@gmail.com>
2024-04-23 19:43:45 -0300

    implemento afd

--- AFD/q0.py
+++ AFD/q0.py
@@ -11,35 +11,117 @@ from AFD.q15 import ver_pt_v
 from AFD.q7 import ver_vir
 
 currentState: str = 'q0'
+concatLexeme: str = ''
 
 def afd(line: list[str]) -> str:
     for lexeme in line:
-        if lexeme == range(0,9):
-            ver_q19(lexeme, currentState)
-        elif lexeme == '"':
-            ver_q5(lexeme, currentState)
-        elif lexeme.isalpha():
-            ver_q6(lexeme, currentState)
-        elif lexeme == "{":
-            ver_q2(lexeme, currentState)
-        elif lexeme == '$':
-            ver_q3()
-        elif lexeme == ("<" or '=' or '>'):
-            ver_q8(lexeme, currentState)
-        elif lexeme == ("+" or '-' or '*' or '/'):
-            ver_q18(lexeme, currentState)
-        elif lexeme == '(':
-            ver_q16(lexeme, currentState)
-        elif lexeme == ')':
-            ver_q15(lexeme, currentState)
-        elif lexeme == ',':
-            ver_q7(lexeme, currentState)
-        else:
-            return "ERRO - afd"
-       
-def ver_q2(lexeme):
+        if currentState == 'q0':
+            q0(lexeme)
+        if currentState == 'q1':
+            concatLexeme += lexeme
+            if lexeme == '}':
+                currentState ='q2'
+                ver_q2(concatLexeme)
+                concatLexeme = ''
+        if currentState == 'q4':
+            concatLexeme += lexeme
+            if lexeme == '"':
+                currentState ='q5'
+                ver_q5(concatLexeme)
+                concatLexeme = ''
+        if currentState == 'q6':
+            if lexeme == range(0,9):
+                concatLexeme += lexeme
+            elif lexeme.isalpha():
+                concatLexeme += lexeme
+            elif lexeme == '_':
+                concatLexeme += lexeme
+            else:
+                ver_q6(concatLexeme)
+                concatLexeme = ''
+                q0(lexeme)
+        if currentState == 'q8':
+            if lexeme == '=' and concatLexeme == '<':
+                concatLexeme += lexeme
+                ver_q8(concatLexeme)
+            elif lexeme == '-' and concatLexeme == '<':
+                concatLexeme += lexeme
+                ver_q10(concatLexeme)
+            elif lexeme == '>' and concatLexeme == '<':
+                concatLexeme += lexeme
+                ver_q8(concatLexeme)
+            elif lexeme == '=' and concatLexeme == '>':
+                concatLexeme += lexeme
+                ver_q8(concatLexeme)
+            else:
+                ver_q8(concatLexeme)
+        if currentState == 'q19':
+            if int(lexeme) == range(0,9):
+                concatLexeme += lexeme
+            if lexeme == '.':
+                currentState = 'q20'
+                concatLexeme += lexeme
+            else:
+                ver_num()
+        if currentState == 'q20':
+            if int(lexeme) == range(0,9):
+                concatLexeme += lexeme
+            if lexeme == 'E' or 'e':
+                concatLexeme += lexeme
+                currentState = 'q21'
+            else:
+                ver_num()
+        if currentState == 'q21':
+            if lexeme == '+' or '-':
+                concatLexeme += lexeme
+                currentState == 'q22'
+            if int(lexeme) == range(0,9):
+                concatLexeme += lexeme
+                currentState = 'q22'
+            else:
+                ver_num()
+        if currentState == 'q22':
+            if int(lexeme) == range(0,9):
+                concatLexeme += lexeme
+            else:
+                ver_num()
+
+def q0(lexeme:str):
+    if int(lexeme) == range(0,9):
+        currentState = 'q19'
+        concatLexeme += lexeme
+    elif lexeme == '"':
+        currentState = 'q4'
+        concatLexeme += lexeme
+    elif lexeme.isalpha():
+        currentState = 'q6'
+        concatLexeme += lexeme
+    elif lexeme == "{":
+        currentState = 'q1'
+        concatLexeme += lexeme
+    elif lexeme == '$':
+        ver_q3()
+    elif lexeme == ("<" or '>'):
+        currentState = 'q8'
+        concatLexeme += lexeme
+    elif lexeme == '=':
+        ver_q12()
+    elif lexeme == ("+" or '-' or '*' or '/'):
+        ver_q18(lexeme)
+    elif lexeme == '(':
+        ver_q16()
+    elif lexeme == ')':
+        ver_q17()
+    elif lexeme == ';':
+        ver_q15()
+    elif lexeme == ',':
+        ver_q7()
+    else:
+        return "ERRO - afd"
+
+def ver_q2(concatLexeme):
     if lexeme == '{':
-        comment = '{'
+        comment:str = '{'
         for word in line:
             comment = comment + ' ' + word
             
@@ -48,15 +130,19 @@ def ver_q2(lexeme):
 
 def ver_q3(lexeme):
     return 'EOF'
+    currentState = 'q0'
 
-def ver_q5(lexeme):
+def ver_q5(concatLexeme):
     print('no script')
+    currentState = 'q0'
 
 def ver_q6(lexeme):
     print('no script')
+    currentState = 'q0'
 
 def ver_q7(lexeme):
     print(currentState)
+    currentState = 'q0'
 
 def ver_q15(lexeme):
     return ('pt_v', ';')
@@ -73,5 +159,11 @@ def ver_q18(lexeme):
 def ver_q19(lexeme):
     return
 
-def ver_q8(lexeme):
-    print('no script')
+def ver_q8(lexeme:str):
+    currentState = 'q8'
+    if lexeme == '<':
+
+        ver_q9()
+        
+def ver_q9(lexeme:str):
+    currentState = 'q9'
