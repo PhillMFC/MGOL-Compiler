cd8c48dd55f8bed9e259a91eb94d43e3397b2d02
phillmfc <phillipemfc2@hotmail.com>
2025-04-03 19:39:22 -0300

    "main file improvement"

--- main/main.py
+++ main/main.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
 
     _scanner = Scanner()
     try:
-        while (token := _scanner.requestToken()):
-            if not isinstance(token, bool):
+        while (token:= _scanner.requestToken()):
+            if not isinstance(token, bool) and token.lexeme != '\n':
                 print('main-scope', token.toString())
             
-                
-    
-        SymbolTable.printTable()
 
     except Exception as e:
        print("código compilado")
+    
+    SymbolTable.printTable()
